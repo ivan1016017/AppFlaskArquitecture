e49a0d4634e254c36ea30eae72b3bf4198bf5d13
Ivan Penaloza <ivan1016017@gmail.com>
2021-09-17 10:22:20 -0500

    2 implementacion de clases

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,26 @@
 from flaskr import create_app
+from .modelos import db, Cancion
 
 app = create_app('default')
 app_context = app.app_context()
+
+# to use context to avoid reuse of the app code in differnet instances through the app
 app_context.push()
+
+db.init_app(app)
+
+# to create all the tables 
+db.create_all() 
+
+
+# test the app
+
+with app.app_context():
+    c1 = Cancion(titulo = "PrimeraPrueba", minutos = 2, segundos = 25, interprete = "ivan")
+    c2  = Cancion(titulo = "SegundaPrueba", minutos = 3, segundos = 12, interprete = "catalina")
+    db.session.add(c1)
+    db.session.add(c2)
+    db.session.commit()
+
+    # to take a query through all the songs
+    print(Cancion.query.all())
--- flaskr/modelos/modelos.py
+++ flaskr/modelos/modelos.py
@@ -7,8 +7,8 @@ db = SQLAlchemy()
 class Cancion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     titulo = db.Column(db.String(128))
-    minutos = db.Columns(db.Integer)
-    segundos = db.Columns(db.Integer)
+    minutos = db.Column(db.Integer)
+    segundos = db.Column(db.Integer)
     interprete = db.Column(db.String(128))
 
     def __repr__(self):
