6939f045d0649c566347daa79e633e7cb374fb46
Ivan Penaloza <ivan1016017@gmail.com>
2021-09-17 13:17:37 -0500

    implementacion de las asociaciones

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,5 @@
 from flaskr import create_app
-from .modelos import db, Cancion
+from .modelos import db, Cancion, Album, Usuario, Medio
 
 app = create_app('default')
 app_context = app.app_context()
@@ -18,9 +18,20 @@ db.create_all()
 with app.app_context():
     c1 = Cancion(titulo = "PrimeraPrueba", minutos = 2, segundos = 25, interprete = "ivan")
     c2  = Cancion(titulo = "SegundaPrueba", minutos = 3, segundos = 12, interprete = "catalina")
-    db.session.add(c1)
-    db.session.add(c2)
+    u = Usuario(nombre = 'Ivan', contrasena = "1234")
+    a = Album(titulo = "albumPrueba", anio = 2020, descripcion = "Este es el nuevo album", medio = Medio.CD)
+    a.canciones.append(c1)
+    a.canciones.append(c2)
+    u.albumes.append(a)
+    db.session.add(u)
     db.session.commit()
+    print(Usuario.query.all())
+    print(Usuario.query.all()[0].albumes)
+    print(Album.query.all()[0].canciones)
+    print(Cancion.query.all())
+    db.session.delete(a)
+    print(Album.query.all())
+    print(Cancion.query.all())
+    print(u)
 
     # to take a query through all the songs
-    print(Cancion.query.all())
