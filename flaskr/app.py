ef0bd3160444aae406788e2d162008ef3561a165
Ivan Penaloza <ivan1016017@gmail.com>
2021-09-17 15:12:32 -0500

    implementacion api-rest

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,6 +1,8 @@
 from flaskr import create_app
 from .modelos import db, Cancion, Album, Usuario, Medio
 from .modelos import AlbumSchema, CancionSchema, UsuarioSchema
+from flask_restful import Api
+from .vistas import VistasCanciones, VistaCancion, VistaLogIn, VistaSignIn, VistaAlbumsUsuario, VistaCancionesAlbum, VistaAlbum
 
 app = create_app('default')
 app_context = app.app_context()
@@ -14,20 +16,7 @@ db.init_app(app)
 db.create_all() 
 
 
-# test the app
+api = Api(app)
 
-with app.app_context():
-    album_schema = AlbumSchema() # it transform the information from pythn to json format
-    cancion_schema = CancionSchema()
-    A = Album(titulo = "prueba", anio = 2020, descripcion = "texto", medio = Medio.CD)
-    C = Cancion( titulo = "cancionPrueba", minutos = 2, segundos = 1, interprete = "interpretePrueba")
-    A.canciones.append(C)
-    db.session.add(C)
-    db.session.add(A)
-    db.session.commit()
-    print([album_schema.dumps(album) for album in Album.query.all()])
-    print([cancion_schema.dumps(cancion) for cancion in Cancion.query.all()])
-
-
-
-    # to take a query through all the songs
+api.add_resource(VistasCanciones, '/canciones')
+api.add_resource(VistaCancion, '/cancion/<int:id_cancion>')
