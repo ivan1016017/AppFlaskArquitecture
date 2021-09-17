d19e2e79c5c1efecece12b913169ae870475db47
Ivan Penaloza <ivan1016017@gmail.com>
2021-09-17 16:08:47 -0500

    changing the name of VistasCanciones for VistaCanciones

--- flaskr/app.py
+++ flaskr/app.py
@@ -2,7 +2,7 @@ from flaskr import create_app
 from .modelos import db, Cancion, Album, Usuario, Medio
 from .modelos import AlbumSchema, CancionSchema, UsuarioSchema
 from flask_restful import Api
-from .vistas import VistasCanciones, VistaCancion, VistaLogIn, VistaSignIn, VistaAlbumsUsuario, VistaCancionesAlbum, VistaAlbum
+from .vistas import VistaCanciones, VistaCancion, VistaLogIn, VistaSignIn, VistaAlbumsUsuario, VistaCancionesAlbum, VistaAlbum
 
 app = create_app('default')
 app_context = app.app_context()
@@ -18,7 +18,7 @@ db.create_all()
 
 api = Api(app)
 
-api.add_resource(VistasCanciones, '/canciones')
+api.add_resource(VistaCanciones, '/canciones')
 api.add_resource(VistaCancion, '/cancion/<int:id_cancion>')
 api.add_resource(VistaSignIn, '/signin')
 api.add_resource(VistaLogIn, '/login')
